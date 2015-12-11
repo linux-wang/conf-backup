a2811537b65dc5eb8aa85c44830662b56dd33b88
wangshengwei <wsw1990wsw@qq.com>
2015-12-12 00:07:27 +0800

    v0.1

--- youdao.py
+++ youdao.py
@@ -1,4 +1,25 @@
 #!/usr/lib/bin
 #Filename:youdao.py
 
-import 
+#coding="utf-8"
+
+import urllib2
+import sys
+
+
+
+def translate(url,word):
+	html=urllib2.urlopen(url+word)
+	print html.read()
+
+
+def main():
+	url="http://fanyi.youdao.com/openapi.do?keyfrom=oldfriends-wang&key=1908356382&type=data&doctype=json&version=1.1&q="
+	word=raw_input("word:")
+	translate(url,word)
+
+
+if __name__=="__main__":
+	main()
+
+
