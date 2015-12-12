2a1a58f27cd289c23bd7cb95e234914434903d93
wangshengwei <wsw1990wsw@qq.com>
2015-12-12 17:14:07 +0800

    still sth unwork

--- youdao.py
+++ youdao.py
@@ -5,17 +5,30 @@
 
 import urllib2
 import sys
+import json
 
-
-
+def print_dict(result):
+	for key,value in result.items():
+		#key=json.dumps(key,encoding='utf-8',ensure_ascii=False)
+#		result=result.read()
+#		result=json.loads(result)
+		if key=='translation':
+			print key,value
+		else:
+			pass
+		
 def translate(url,word):
 	html=urllib2.urlopen(url+word)
-#	print "result:"+html.read()
-	result=html.read()
-	print lens(result)	
-	for name,id in result.items():
-		print (name,id)
-	print result
+	print "result:"+html.read()
+
+#	print_dict(result)
+
+#	result=json.dumps(result,ensure_ascii=False,encoding='gb2312',indent=None)
+
+#	print type(result)
+
+
+#	print result
 
 def main():
 	url="http://fanyi.youdao.com/openapi.do?keyfrom=oldfriends-wang&key=1908356382&type=data&doctype=json&version=1.1&q="
